bd1d0fe24e2e01c25ccf54975b77a8543f99525f
Matt Maatubang <maatubangl@hotmail.com>
2020-02-10 11:07:54 -0800

    removed data-ingestion -> moved to data_ingestion

--- data-ingestion/utilities/move_from_local_to_s3.py
+++
@@ -1,8 +0,0 @@
-'''
-Module to move file from local to S3
-'''
-import s3_file_manager
-
-S3FM = s3_file_manager.s3_file_manager()
-S3FM.set_bucket_name("assessor-data-bucket")
-S3FM.put_file("LA_2019.parquet")

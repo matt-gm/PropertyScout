e151718898ec5d090cee55ac5afa425868b39a73
Matt Maatubang <maatubangl@hotmail.com>
2020-02-11 11:40:28 -0800

    app updates and creating requirements.txt

--- frontend/app.py
+++ frontend/app.py
@@ -9,7 +9,7 @@ import numpy as np
 app = dash.Dash(__name__)
 app.config.suppress_callback_exceptions = True
 
-
+# Global Vaiables to control what data is stored
 la_data = pd.read_csv('la_data_new.csv')
 cd = la_data
 selectable_cities = cd.city.unique()
@@ -18,6 +18,7 @@ selectable_streets = cd.street.unique()
 cities = ["Los Angeles", "San Francisco", "New York City", "Denver",
           "Las Vegas", "Austin"]
 
+# Main App
 app.layout = html.Div([
     html.H1("PropertyScout", style={"textAlign": "center"}),
 
@@ -36,7 +37,7 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='city-dropdown',
                 options=[{'label': i, 'value': i} for i in selectable_cities],
-                value=selectable_cities
+                value=None
             )
         ],
         style={'width': '33%', 'display': 'inline-block'}),
@@ -46,7 +47,7 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='neighborhood-dropdown',
                 options=[{'label': i, 'value': i} for i in selectable_neighborhoods],
-                value=selectable_neighborhoods
+                value=None
             )],
             style={'width': '33%', 'display': 'inline-block'}
         ),
@@ -56,7 +57,7 @@ app.layout = html.Div([
             dcc.Dropdown(
                 id='street-dropdown',
                 options=[{'label': i, 'value': i} for i in selectable_streets],
-                value=selectable_streets
+                value=None
             )
         ],
         style={'width': '33%', 'display': 'inline-block'}),
@@ -67,14 +68,14 @@ app.layout = html.Div([
             id='assessor-values-over-time-series'
             #,hoverData={'points':}
         )]
-    ),
+    )#,
 
-    html.Div([
-        dcc.Graph(
-            id='assessor-values-by-type-bars'
-            #,hoverData={'points':}
-        )]
-    )
+    #html.Div([
+    #    dcc.Graph(
+    #        id='assessor-values-by-type-bars'
+    #        #,hoverData={'points':}
+    #    )]
+    #)
 ])
 
 
@@ -83,15 +84,35 @@ def create_time_series(dff, title):
         'data': [dict(
             x=dff['year'].unique(),
             y=(dff['avg_land_value'] + dff['avg_improvement_value']),
-            mode='lines+markers'
+            mode='lines+markers',
+            name='Average Total Value'
+        ),
+        dict(
+            x=dff['year'].unique(),
+            y=(dff['avg_land_value']),
+            mode='lines+markers',
+            name='Average Land Value'
+        ),
+        dict(
+            x=dff['year'].unique(),
+            y=(dff['avg_improvement_value']),
+            mode='lines+markers',
+            name='Average Improvement Value'
         )],
         'layout': {
             'height': 700,
-            'margin': {'l': 40, 'b': 40, 'r': 20, 't': 10},
+            'margin': {'l': 60, 'b': 40, 'r': 20, 't': 40},
+            'xaxis': {
+                'title': "Years"
+            },
+            'yaxis': {
+                'title': "Assessed Value in USD"
+            },
             'annotations': [{
-                'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
+                'y': 1, 'xanchor': 'left', 'yanchor': 'bottom',
                 'xref': 'paper', 'yref': 'paper', 'showarrow': False,
-                'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
+                'align': 'left',
+                'bgcolor': 'rgba(255, 255, 255, 0.5)',
                 'text': title
             }]
         }
@@ -108,17 +129,16 @@ def create_bar_chart(dff, title):
      Input('street-dropdown', 'value')])
 def update_scatter(city, neighborhood, street):
     selected_data = cd
-    title_str = "Los Angeles Assessed Value"
+    title_str = "Los Angeles County Assessor Value"
     if isSelected(city):
         selected_data = selected_data.loc[selected_data['city'] == city]
-        title_str += " in " + city
+        title_str = city + " Assessor Value"
     if isSelected(neighborhood):
         selected_data = selected_data.loc[selected_data['neighborhood'] == neighborhood]
         title_str += " in " + str(neighborhood)
     if isSelected(street):
         selected_data = selected_data.loc[selected_data['street'] == street]
         title_str += " on " + street
-
     if selected_data.empty:
         title_str = "No matching data."
 
@@ -141,6 +161,7 @@ def update_scatter(city, neighborhood, street):
 
 def isSelected(in_val):
     return not isinstance(in_val, list) and (in_val is not None)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
